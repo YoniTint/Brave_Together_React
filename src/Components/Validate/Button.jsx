bc59c597d0d765ce4323900dc5911b7dc8b885dc
ruth <ruthgut97@gmail.com>
2022-04-19 13:56:43 +0300

    buttons added

--- src/Components/Validate/Button.jsx
+++ src/Components/Validate/Button.jsx
@@ -1,31 +1,82 @@
-import React from 'react'
-import checkButton from './media/checkButton.png'
+import React from "react";
+import checkButton from "./media/checkButton.svg";
+import offensiveButton from "./media/Button-cancel.svg";
 
-export default function Button() {
-    return (
-        <div style={buttonStyle}>
-            <div style={display:flex}>
-                <img
-                style={imgStyle},
-
-            </div>
-            
-        </div>
-    )
+export default function Button({ isOffensive }) {
+  return (
+    <div
+      style={
+        isOffensive
+          ? { ...buttonStyle, backgroundColor: "#FF5B3C" }
+          : { ...buttonStyle }
+      }
+    >
+      <img
+        style={
+          isOffensive
+            ? { ...imgStyle, border: "2px solid #FF5B3C" }
+            : { ...imgStyle }
+        }
+        src={isOffensive ? offensiveButton : checkButton}
+      />
+      {isOffensive ? (
+        <span style={offensiveStyle}>Offensive</span>
+      ) : (
+        <span style={safeStyle}>Safe</span>
+      )}
+    </div>
+  );
 }
 
+const safeStyle = {
+  height: 22,
+  width: 35,
+  left: 69,
+  top: 16,
+  borderRadius: null,
+  fontFamily: "Open Sans",
+  fontStyle: "normal",
+  fontWeight: "700",
+  fontSize: 16,
+  color: "#FFFFFF",
+};
+
+//backgroundColor: rgba(109, 209, 166, 1)
 const buttonStyle = {
-    backgroundColor:rgba(109, 209, 166, 1),
-    borderRadius:50,
-}
+  backgroundColor: "#29CC97",
+  borderRadius: 15,
+  height: 45,
+  width: 162,
+  left: 0,
+  top: 0,
+  gap: 10,
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
-const titleStyle = {
-    
-}
+//left: "calc(50% - 10.5px/2 - 0.15px)",
+//top: "calc(50% - 7px/2 - 0.5px)",
 
 const imgStyle = {
-    marginTop:17,
-    marginLeft:39,
-    width:10.5,
-    height:7,
-}
+  backgroundColor: "white",
+  width: 20,
+  height: 20,
+  left: 39,
+  top: 17,
+  borderRadius: 20,
+  padding: "4px,16px,4px,16px",
+
+  border: "2px solid #6DD1A6",
+};
+
+const offensiveStyle = {
+  padding: "none",
+  borderRadius: null,
+  fontFamily: "Open Sans",
+  fontStyle: "normal",
+  fontWeight: "700",
+  fontSize: 16,
+  color: "#FFFFFF",
+};
--- src/Components/Validate/ValidateCard.jsx
+++ src/Components/Validate/ValidateCard.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import Button from "./Button";
 
 export default function ValidateCard() {
-  return <div>asdasd</div>;
+  return (
+    <div style={cardStyle}>
+      <div style={buttonsStyle}>
+        <Button />
+        <Button isOffensive={true} />
+      </div>
+    </div>
+  );
 }
+
+const cardStyle = {
+  height: 694,
+  width: 499,
+  left: 892,
+  top: 129,
+  borderRadius: 25,
+  padding: 24,
+  border: "solid",
+  backgroundColor: "white",
+};
+
+const buttonsStyle = {
+  display: "flex",
+  justifyContent: "center",
+  gap: 16,
+};
