88e94af107102252578ec33ca2b19a0f60f189b3
Alon Ronder <68230416+Alpha110R@users.noreply.github.com>
2022-06-07 17:20:45 +0300

    Alon: Update the postCard's title

--- src/Components/Validate/PostInfo.jsx
+++ src/Components/Validate/PostInfo.jsx
@@ -6,8 +6,11 @@ import star from "./media/star.svg";
 import arrow from "./media/arrow.svg";
 
 export default function PostInfo({ platform, date }) {
-  const info = `Post from: ${platform} | Posted: ${date}`;
-  let platformImage = null;
+  const info = `Post from: ${platform} | Posted: ${dateFormat(
+    date,
+    "dd-MM-yyyy"
+  )}`;
+  var platformImage;
   switch (platform) {
     case "facebook":
       platformImage = facebookLogo;
@@ -23,33 +26,49 @@ export default function PostInfo({ platform, date }) {
   }
   return (
     <Info>
-      <Icon src={platformImage} />
+      <PlatformIcon src={platformImage} />
       <TitleStyle>{info}</TitleStyle>
-      <SideIcon src={arrow} />
-      <Icon src={star} />
+      <SideIcons>
+        <IconArea>
+          <RigthIcon src={arrow} />
+        </IconArea>
+        <IconArea>
+          <RigthIcon src={star} />
+        </IconArea>
+      </SideIcons>
     </Info>
   );
 }
 
-const SideIcon = styled.img`
-  background-color: #eeeeee;
-  border-radius: 20px;
-  padding: 4px;
-  height: 26px;
-  width: 26px;
+const SideIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
   margin-left: auto;
 `;
 
-const Icon = styled.img`
+const IconArea = styled.div`
   background-color: #eeeeee;
   border-radius: 20px;
   padding: 4px;
   width: 26px;
   height: 26px;
+  margin-left: 8px;
+  text-align: center;
+`;
+
+const PlatformIcon = styled.img`
+  width: 26px;
+`;
+
+const RigthIcon = styled.img`
+  width: 13px;
+  opacity: 0.9;
+  color: black;
 `;
 
 const Info = styled.div`
   display: flex;
+  align-items: center;
 `;
 
 const TitleStyle = styled.span`
@@ -61,4 +80,28 @@ const TitleStyle = styled.span`
   color: #091430;
   margin-left: 8px;
   margin-top: 4.5px;
+  text-align: center;
 `;
+
+function dateFormat(inputDate, format) {
+  const date = new Date(inputDate);
+
+  //extract the parts of the date
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  //replace the month
+  format = format.replace("MM", month.toString().padStart(2, "0"));
+
+  //replace the year
+  if (format.indexOf("yyyy") > -1) {
+    format = format.replace("yyyy", year.toString());
+  } else if (format.indexOf("yy") > -1) {
+    format = format.replace("yy", year.toString().substr(2, 2));
+  }
+  //replace the day
+  format = format.replace("dd", day.toString().padStart(2, "0"));
+
+  return format;
+}
