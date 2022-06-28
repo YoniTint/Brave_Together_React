d6e537666a6365ae2349557f5fc336a4aa934cfc
Benny Mosheyev <bennymsv@gmail.com>
2022-06-28 17:06:16 +0300

    Thank You Window progress

--- src/Components/Validate/ThankYouWindow.jsx
+++ src/Components/Validate/ThankYouWindow.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import thanks from "./media/thanks.svg";
+import reasonsCheckbox from "./media/reasons_checkbox.svg";
 
 export default function ThankYouWindow() {
   const thankNote =
@@ -10,28 +11,68 @@ export default function ThankYouWindow() {
 
   return (
     <>
-      <Thank>
-        Thank You For Your Report!
+      <Wrapper>
+        <div style={{ weight: 800, marginTop: 65 }}>
+          Thank You For Your Report!
+        </div>
         <ThankImage src={thanks} />
         <ThankNote>{thankNote}</ThankNote>
         <hr
           style={{
             backgroundColor: "grey",
             width: 515,
-            marginLeft: 0,
+            marginLeft: 80,
+            marginRight: 80,
             marginTop: 64,
             marginBottom: 45,
           }}
         />
-      </Thank>
+        <span style={{ weight: 600, fontSize: 18 }}>
+          {" "}
+          <span style={{ weight: 900, fontSize: 24 }}>Most</span> of the users
+          think this post is offensive
+        </span>
+        <span
+          style={{
+            fontSize: 14,
+            marginTop: 32,
+            marginLeft: 144,
+            marginRight: 144,
+            textAlign: "center",
+            marginBottom: 24,
+          }}
+        >
+          These are the reasons why more people thought the post is offensive:
+        </span>
+
+        <div style={{ display: "flex" }}>
+          <img src={reasonsCheckbox} style={{ marginRight: 16 }} />
+          <span style={{ fontSize: 14 }}>Minimizing the holocaust</span>
+        </div>
+
+        <hr
+          style={{
+            backgroundColor: "grey",
+            width: 515,
+            marginLeft: 80,
+            marginRight: 80,
+            marginTop: 64,
+            marginBottom: 45,
+          }}
+        />
+      </Wrapper>
     </>
   );
 }
 
-const Thank = styled.div`
-  position: absolute;
-  width: 323px;
-  top: 65px;
+const Wrapper = styled.div`
+  position: fixed;
+  right: 0;
+  width: 675px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  height: 133vh;
 
   font-family: "Inter";
   font-style: normal;
@@ -42,17 +83,17 @@ const Thank = styled.div`
 `;
 
 const ThankImage = styled.img`
-  position: absolute;
+  margin-top: 40px;
+  margin-bottom: 16px;
+  /* position: absolute;
   left: 42.35%;
   right: 42.35%;
   top: 0%;
-  bottom: 49.59%;
+  bottom: 49.59%; */
 `;
 
 const ThankNote = styled.div`
-  position: absolute;
   width: 327px;
-  top: 77px;
   font-family: "Inter";
   font-style: normal;
   font-weight: 600;
