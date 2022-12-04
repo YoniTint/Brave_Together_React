468ed42d881b3f6163b4d3ea834d3139aad4545f
I561817 <yonatan.tintpulver@sap.com>
2022-12-04 23:20:06 +0200

    NEED TO COMPLETE FORM DATA SENDING

--- src/Components/Hooks/postReport.jsx
+++ src/Components/Hooks/postReport.jsx
@@ -2,5 +2,10 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 export default async function postReport(URL, reasons, other) {
-  return await axios.post("api/report", { URL, reasons, other });
+    return await axios.post("http://localhost:1337/api/decisions", {
+      data: {
+        volunteerDecision: true,
+        reasons: Object.keys(reasons)
+      }
+  });
 }
